e2dd83a6fb0128159badc6021e9c399127b5cd3e
Piggey <pepcio3320@gmail.com>
2023-06-06 23:51:00 +0200

    init spring boot project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # spring-boot-rest-api
+
+## Features
+- JWT security
+- PostgreSQL database
+- Dockerfile
+- docker-compose for app and database containers
+- Bearer token guarded endpoints
+- Swagger docs
---
+++ src/main/java/com/piggey/cars/user/User.java
@@ -0,0 +1,60 @@
+package com.piggey.cars.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return firstname + " " + lastname;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
